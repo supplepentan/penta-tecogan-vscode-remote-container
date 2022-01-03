ce5bf850cd25377d03a951f353c23b1d5d653320
supplepentan <pentagramog@gmail.com>
2022-01-04 08:53:33 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 pip install --user nvidia-pyindex
 pip install --user nvidia-tensorflow[horovod]
 pip install keras==2.3.1
+pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
